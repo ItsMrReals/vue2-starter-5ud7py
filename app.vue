60d09087799a6ca4894c1da3808cf443321662d1
ItsMrReals <asherhayesgreen@gmail.com>
2021-07-24 19:59:26 -0600

    Initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template> 
+<div id="app">
+		<h1>My Todo App!</h1>
+		<dog dog="woof"/>
+	</div>
+</template>
+
+<script>
+import dog from './components/dog.vue';
+
+export default {
+  components: {
+    dog
+  }
+}
+</script>
---
+++ components/dog.vue
@@ -0,0 +1,12 @@
+<template>
+<h1> hi </h1>
+</template>
+
+<script> 
+export default {
+  name: "woof"
+  props: {
+    dog: String
+  }
+}
+</script>
